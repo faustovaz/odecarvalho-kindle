12895383611e0f4663cc675a52ae11f9f29e86f3
Jefferson Fausto Vaz <faustovaz@gmail.com>
2013-09-30 06:51:28 +0000

    Added the feature that scrapp article data and article text

--- bin/ArticleScrapper.py
+++ bin/ArticleScrapper.py
@@ -4,28 +4,66 @@ from bs4 import BeautifulSoup
 class ArticleScrapper:
 	
 	def __init__(self):
-		self.url = urllib.urlopen("http://www.olavodecarvalho.org/index.html")
-		self.beautifulSoup = BeautifulSoup(self.url)
-		self.articles = []
+		self.address = "http://www.olavodecarvalho.org/"
+		self.fullArticles = []
 
-	def loadAllArticlesData(self):
-		tables = self.beautifulSoup.find_all('table')
+	def getScrapper(self, link):
+		url = urllib.urlopen(link)
+		return BeautifulSoup(url)
+
+	def getAllArticlesListed(self):
+		articles = []
+		beautifulSoup = self.getScrapper(self.address + "index.html")
+		tables = beautifulSoup.find_all('table')
 		articleTable = tables[7]
 		allLinks = articleTable.find_all('a', {'class' : 'menulink'})
 		for link in allLinks:
 			words = link.text.splitlines()
 			articleName = " ".join(words)
-			self.articles.append({'name' : articleName, 'link' : link['href']})
+			articles.append({'name' : articleName.encode('utf-8'), 'link' : link['href']})
+		return articles
 
-	def start(self):
-		pass
+	def getFullArticlesData(self):
+		articles = self.getAllArticlesListed()
+		for article in articles:
+			beautifulSoup = self.getScrapper(self.address + article['link'])
+			table = beautifulSoup.find('table')
+			table_tds = table.find_all('td')
+			td = table_tds[1]
+			paragraphs = td.find_all('p')
+			divs = td.find_all('div')
 
-	def printArticleData(self):
-		for article in self.articles:
-			print article['name'] , " , " , article['link']
+			if len(divs) > 0:
+				html_title = paragraphs[1]
+				html_date = paragraphs[2]
+				html_article = divs[0]
+			
+				title = html_title.text.split("\n")
+				article_data = html_date.text.split("\n")
+			
+				title = title[0]
+				newspaper = article_data[0]
+				date = article_data[1]
+
+				self.fullArticles.append({	'title' : unicode(title), 
+											'date' : unicode(date), 
+											'newspaper' : unicode(newspaper), 
+											'text' : unicode(html_article)
+										})
+
+
+
+
+	def printAllData(self):
+		self.getFullArticlesData()
+		for article in self.fullArticles:
+			print article['title']
+			print article['date']
+			print article['newspaper']
+			print '\n\n'
 
 
 if __name__ == '__main__':
-	scrapper = ArticleScrapper()
-	scrapper.loadAllArticlesData()
-	scrapper.printArticleData()
+	a = ArticleScrapper()
+	a.printAllData()
+
